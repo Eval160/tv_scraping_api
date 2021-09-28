3c25f382b4619f00d4d162b490a3e82cc1a9c1e6
Valentine Ecrepont <58532138+Eval160@users.noreply.github.com>
2021-09-28 16:20:30 +0200

    add log to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,12 @@ url = 'https://xmltv.ch/xmltv/xmltv-complet.xml'
 # url = 'https://xmltv.ch/xmltv/xmltv-tnt.xml'
 xml = Nokogiri::XML(URI.open(url))
 channels = Hash.from_xml(xml.to_s)['tv']['channel']
+puts "Channels array:"
+puts channels
+
 channels.each do |chan|
+  puts "******"
+  puts chan
   Channel.create!(telerama_id: chan['id'], name: chan['display_name'], icon: chan['icon']['src'])
 end
 puts "Channels created"
