e99c439097b6c6aedfecd54b4c4007ab372c9f35
Valentine Ecrepont <58532138+Eval160@users.noreply.github.com>
2021-09-29 14:31:11 +0200

    add scope to model + search by params

--- app/controllers/api/v1/programs_controller.rb
+++ app/controllers/api/v1/programs_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProgramsController < ApplicationController
   def index
-    @programs = Program.all
+    @programs = Program.search(params)
     render json: ProgramSerializer.new(@programs).serializable_hash
   end
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,4 +1,15 @@
 class Program < ApplicationRecord
   belongs_to :channel
   validates :title, :start_time, :stop_time, presence: true
+  default_scope { order("start_time ASC") }
+
+  scope :daily_programs, -> { where('stop_time > ? ', Time.now).where('start_time > ? ', Time.now + 1.day) }
+  scope :filter_by_channel, -> (telerama_id) { joins(:channel).where('channels.telerama_id' => telerama_id) }
+
+  def self.search(params = {})
+    programs = Program.all
+    programs = programs.daily_programs if params[:daily]
+    programs = programs.filter_by_channel(params[:channel]) if params[:channel]
+    programs
+  end
 end
