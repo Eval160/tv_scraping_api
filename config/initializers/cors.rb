ef3f7005953ce7ae3844b97980e65af87e846bea
Valentine Ecrepont <58532138+Eval160@users.noreply.github.com>
2021-09-30 10:31:50 +0200

    allow all origin for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'rit-tv-guide-api.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
