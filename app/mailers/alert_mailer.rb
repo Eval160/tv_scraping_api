42ea57998f36e2f87f9941c72073782f1e0bb31a
Valentine Ecrepont <58532138+Eval160@users.noreply.github.com>
2021-11-03 11:01:54 +0100

    test other mail

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -2,6 +2,6 @@ class AlertMailer < ApplicationMailer
   default from: 'ecrepontvalentine@gmail.com'
 
   def program_scraping_error
-    mail(to: "ecrepontvalentine@gmail.com", subject: "Le scraping des programmes n'a pas fonctionné")
+    mail(to: "sylvaingaltier@roomintouch.fr", subject: "Le scraping des programmes n'a pas fonctionné")
   end
 end
